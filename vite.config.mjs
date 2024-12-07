8b11d4682e832be49d3906a0e6b5edf5ce957210
Kauan Hindlmayer <kauanhindlmayer07@gmail.com>
2024-12-07 13:53:08 -0300

    feat: add vue devtools plugin

--- vite.config.mjs
+++ vite.config.mjs
@@ -4,6 +4,7 @@ import { PrimeVueResolver } from '@primevue/auto-import-resolver';
 import vue from '@vitejs/plugin-vue';
 import Components from 'unplugin-vue-components/vite';
 import { defineConfig } from 'vite';
+import vueDevTools from 'vite-plugin-vue-devtools';
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -13,6 +14,7 @@ export default defineConfig({
     },
     plugins: [
         vue(),
+        vueDevTools(),
         Components({
             resolvers: [PrimeVueResolver()]
         })
