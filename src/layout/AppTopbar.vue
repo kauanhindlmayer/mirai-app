38c0ceb2547134f5b455548ae5baa9c99039fee9
Kauan Hindlmayer <kauanhindlmayer07@gmail.com>
2025-01-14 20:23:14 -0300

    refactor: update topbar menu items

--- src/layout/AppTopbar.vue
+++ src/layout/AppTopbar.vue
@@ -2,14 +2,43 @@
 import { useLayout } from '@/layout/composables/layout'
 import { useUserStore } from '@/stores/user'
 import { storeToRefs } from 'pinia'
+import { ref } from 'vue'
 import AppBreadcrumb from './AppBreadcrumb.vue'
 
 const store = useUserStore()
 const { user } = storeToRefs(store)
 const { layoutState, isDarkTheme, onMenuToggle, onConfigSidebarToggle } = useLayout()
 
+const tabs = [
+  {
+    id: '0',
+    label: 'Work Items',
+    disabled: false,
+  },
+  {
+    id: '1',
+    label: 'Pull Requests',
+    disabled: true,
+  },
+  {
+    id: '2',
+    label: 'Favorites',
+    disabled: true,
+  },
+]
+
+const selectedTab = ref(tabs[0].id)
+
 function toggleSearchBar() {
   layoutState.searchBarActive = !layoutState.searchBarActive
+}
+
+function redirectToDocs() {
+  window.open('https://miraihq.com/en', '_blank')
+}
+
+function contactSupport() {
+  window.open('mailto:support@miraihq.com', '_blank')
 }
 
 function logout() {
@@ -40,21 +69,156 @@ function logout() {
             <i class="pi pi-search" />
           </a>
         </li>
-        <li class="right-sidebar-item">
-          <a class="app-config-button">
+
+        <li class="right-sidebar-item static sm:relative">
+          <a
+            v-styleclass="{
+              selector: '@next',
+              enterFromClass: 'hidden',
+              enterActiveClass: 'animate-scalein',
+              leaveActiveClass: 'animate-fadeout',
+              leaveToClass: 'hidden',
+              hideOnOutsideClick: true,
+            }"
+            class="right-sidebar-button relative z-50"
+          >
             <i class="pi pi-list-check" />
           </a>
+          <div
+            class="list-none m-0 rounded-lg border border-surface absolute bg-surface-0 dark:bg-surface-900 overflow-hidden hidden origin-top min-w-72 sm:w-[22rem] mt-2 right-0 z-50 top-auto shadow-[0px_56px_16px_0px_rgba(0,0,0,0.00),0px_36px_14px_0px_rgba(0,0,0,0.01),0px_20px_12px_0px_rgba(0,0,0,0.02),0px_9px_9px_0px_rgba(0,0,0,0.03),0px_2px_5px_0px_rgba(0,0,0,0.04)]"
+          >
+            <div class="flex items-center border-b border-surface">
+              <button
+                v-for="(tab, index) of tabs"
+                :key="index"
+                @click="selectedTab = tab.id"
+                :class="[
+                  selectedTab === tab.id
+                    ? 'border-surface-950 dark:border-surface-0'
+                    : 'border-transparent',
+                  tab.disabled ? 'opacity-50 pointer-events-none cursor-not-allowed' : '',
+                ]"
+                class="px-3.5 py-2 inline-flex items-center border-b gap-2"
+              >
+                <span
+                  :class="selectedTab === tab.id ? 'text-surface-950 dark:text-surface-0' : ''"
+                  class="label-small"
+                >
+                  {{ tab.label }}
+                </span>
+              </button>
+            </div>
+            <ul
+              class="flex flex-col divide-y divide-[var(--surface-border)] max-h-80 overflow-auto"
+            >
+              <li class="flex justify-center items-center py-4 text-center text-gray-600">
+                <span>No Work Items Found</span>
+              </li>
+            </ul>
+          </div>
         </li>
 
-        <li class="right-sidebar-item">
-          <a class="app-config-button">
+        <li class="right-sidebar-item static sm:relative">
+          <a
+            v-styleclass="{
+              selector: '@next',
+              enterFromClass: 'hidden',
+              enterActiveClass: 'animate-scalein',
+              leaveActiveClass: 'animate-fadeout',
+              leaveToClass: 'hidden',
+              hideOnOutsideClick: true,
+            }"
+            class="right-sidebar-button relative z-50"
+          >
             <i class="pi pi-question-circle" />
           </a>
+          <div
+            class="list-none p-2 m-0 rounded-lg border border-surface overflow-hidden absolute bg-surface-0 dark:bg-surface-900 hidden origin-top w-52 mt-2 right-0 z-[999] top-auto shadow-[0px_56px_16px_0px_rgba(0,0,0,0.00),0px_36px_14px_0px_rgba(0,0,0,0.01),0px_20px_12px_0px_rgba(0,0,0,0.02),0px_9px_9px_0px_rgba(0,0,0,0.03),0px_2px_5px_0px_rgba(0,0,0,0.04)]"
+          >
+            <ul class="flex flex-col gap-1">
+              <li>
+                <a
+                  class="label-small dark:text-surface-400 flex gap-2 py-2 px-2.5 rounded-lg items-center hover:bg-emphasis transition-colors duration-150 cursor-pointer"
+                  @click="redirectToDocs"
+                >
+                  <i class="pi pi-external-link" />
+                  <span>Get Started</span>
+                </a>
+              </li>
+              <li>
+                <a
+                  class="label-small dark:text-surface-400 flex gap-2 py-2 px-2.5 rounded-lg items-center opacity-50 pointer-events-none cursor-not-allowed"
+                >
+                  <i class="pi pi-info-circle" />
+                  <span>Service Status</span>
+                </a>
+              </li>
+              <li>
+                <a
+                  class="label-small dark:text-surface-400 flex gap-2 py-2 px-2.5 rounded-lg items-center hover:bg-emphasis transition-colors duration-150 cursor-pointer"
+                  @click="contactSupport"
+                >
+                  <i class="pi pi-envelope" />
+                  <span>Contact Support</span>
+                </a>
+              </li>
+              <li>
+                <a
+                  class="label-small dark:text-surface-400 flex gap-2 py-2 px-2.5 rounded-lg items-center opacity-50 pointer-events-none cursor-not-allowed"
+                >
+                  <i class="pi pi-lock" />
+                  <span>Privacy Policy</span>
+                </a>
+              </li>
+            </ul>
+          </div>
         </li>
-        <li class="right-sidebar-item">
-          <a class="app-config-button" @click="onConfigSidebarToggle">
+
+        <li class="right-sidebar-item static sm:relative">
+          <a
+            v-styleclass="{
+              selector: '@next',
+              enterFromClass: 'hidden',
+              enterActiveClass: 'animate-scalein',
+              leaveActiveClass: 'animate-fadeout',
+              leaveToClass: 'hidden',
+              hideOnOutsideClick: true,
+            }"
+            class="right-sidebar-button relative z-50"
+          >
             <i class="pi pi-cog" />
           </a>
+          <div
+            class="list-none p-2 m-0 rounded-lg border border-surface overflow-hidden absolute bg-surface-0 dark:bg-surface-900 hidden origin-top w-52 mt-2 right-0 z-[999] top-auto shadow-[0px_56px_16px_0px_rgba(0,0,0,0.00),0px_36px_14px_0px_rgba(0,0,0,0.01),0px_20px_12px_0px_rgba(0,0,0,0.02),0px_9px_9px_0px_rgba(0,0,0,0.03),0px_2px_5px_0px_rgba(0,0,0,0.04)]"
+          >
+            <ul class="flex flex-col gap-1">
+              <li>
+                <a
+                  class="label-small dark:text-surface-400 flex gap-2 py-2 px-2.5 rounded-lg items-center opacity-50 pointer-events-none cursor-not-allowed"
+                >
+                  <i class="pi pi-user" />
+                  <span>Profile</span>
+                </a>
+              </li>
+              <li>
+                <a
+                  class="label-small dark:text-surface-400 flex gap-2 py-2 px-2.5 rounded-lg items-center opacity-50 pointer-events-none cursor-not-allowed"
+                >
+                  <i class="pi pi-clock" />
+                  <span>Time and Locale</span>
+                </a>
+              </li>
+              <li>
+                <a
+                  class="label-small dark:text-surface-400 flex gap-2 py-2 px-2.5 rounded-lg items-center hover:bg-emphasis transition-colors duration-150 cursor-pointer"
+                  @click="onConfigSidebarToggle"
+                >
+                  <i class="pi pi-palette" />
+                  <span>Theme</span>
+                </a>
+              </li>
+            </ul>
+          </div>
         </li>
 
         <li class="profile-item static sm:relative">
@@ -77,41 +241,25 @@ function logout() {
             />
           </a>
           <div
-            class="list-none p-2 m-0 rounded-2xl border border-surface overflow-hidden absolute bg-surface-0 dark:bg-surface-900 hidden origin-top w-52 mt-2 right-0 z-[999] top-auto shadow-[0px_56px_16px_0px_rgba(0,0,0,0.00),0px_36px_14px_0px_rgba(0,0,0,0.01),0px_20px_12px_0px_rgba(0,0,0,0.02),0px_9px_9px_0px_rgba(0,0,0,0.03),0px_2px_5px_0px_rgba(0,0,0,0.04)]"
+            class="list-none p-2 m-0 rounded-lg border border-surface overflow-hidden absolute bg-surface-0 dark:bg-surface-900 hidden origin-top mt-2 right-0 z-[999] top-auto shadow-[0px_56px_16px_0px_rgba(0,0,0,0.00),0px_36px_14px_0px_rgba(0,0,0,0.01),0px_20px_12px_0px_rgba(0,0,0,0.02),0px_9px_9px_0px_rgba(0,0,0,0.03),0px_2px_5px_0px_rgba(0,0,0,0.04)]"
           >
+            <div class="flex items-center gap-3 p-4">
+              <Avatar
+                :image="user?.imageUrl || undefined"
+                :icon="user?.imageUrl ? undefined : 'pi pi-user'"
+                size="xlarge"
+                pt:image:class="!rounded-lg"
+              />
+              <div class="flex flex-col">
+                <span class="text-lg font-semibold text-surface-900 dark:text-surface-0">
+                  {{ user?.fullName }}
+                </span>
+                <span class="text-surface-600 dark:text-surface-200 text-sm">
+                  {{ user?.email }}
+                </span>
+              </div>
+            </div>
             <ul class="flex flex-col gap-1">
-              <li>
-                <a
-                  class="label-small dark:text-surface-400 flex gap-2 py-2 px-2.5 rounded-lg items-center hover:bg-emphasis transition-colors duration-150 cursor-pointer"
-                >
-                  <i class="pi pi-user" />
-                  <span>Profile</span>
-                </a>
-              </li>
-              <li>
-                <a
-                  class="label-small dark:text-surface-400 flex gap-2 py-2 px-2.5 rounded-lg items-center hover:bg-emphasis transition-colors duration-150 cursor-pointer"
-                >
-                  <i class="pi pi-cog" />
-                  <span>Settings</span>
-                </a>
-              </li>
-              <li>
-                <a
-                  class="label-small dark:text-surface-400 flex gap-2 py-2 px-2.5 rounded-lg items-center hover:bg-emphasis transition-colors duration-150 cursor-pointer"
-                >
-                  <i class="pi pi-calendar" />
-                  <span>Calendar</span>
-                </a>
-              </li>
-              <li>
-                <a
-                  class="label-small dark:text-surface-400 flex gap-2 py-2 px-2.5 rounded-lg items-center hover:bg-emphasis transition-colors duration-150 cursor-pointer"
-                >
-                  <i class="pi pi-inbox" />
-                  <span>Inbox</span>
-                </a>
-              </li>
               <li>
                 <a
                   class="label-small dark:text-surface-400 flex gap-2 py-2 px-2.5 rounded-lg items-center hover:bg-emphasis transition-colors duration-150 cursor-pointer"
